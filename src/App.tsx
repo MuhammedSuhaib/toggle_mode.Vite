7578014189b54f546a41006425484af5dd735768
MuhammedSuhaib <umaializa577@gmail.com>
2025-07-21 22:02:46 +0500

    eg

--- src/App.tsx
+++ src/App.tsx
@@ -27,6 +27,7 @@ function App() {    // State for theme mode (true = dark, false = light)
       className={`flex flex-col items-center justify-center min-h-screen transition-all duration-300 ${isDarkMode ? "bg-gray-900 text-white" : "bg-white text-gray-900"
         }`}
     >
+      Simple React UseState Eg
       <button
         type="button"
         className="px-4 py-2 border rounded-md transition-all duration-300 hover:opacity-80"
